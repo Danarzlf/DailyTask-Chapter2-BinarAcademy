71f4226e86d523f55264f4a74cdc997258a8789d
Danar Zulfian Wirakusumah <127115867+Danarzlf@users.noreply.github.com>
2023-03-15 18:08:50 +0700

    Daily Task kedua diChapter 2

---
+++ dailytask2.js
@@ -0,0 +1,466 @@
+let person = 
+[
+    {
+      "_id": "6410370d81bf660621074ab0",
+      "age": 38,
+      "eyeColor": "green",
+      "name": "Walsh Juarez",
+      "address": "Bali",
+      "registered": "2022-09-07T02:39:25 -07:00",
+      "friends": [
+        {
+          "id": 0,
+          "name": "Farid",
+          "isActive": false,
+          "favoriteFruit": "strawberry",
+          "gender": "male",
+          "hobby": [
+            {
+              "id": 2,
+              "hobby": "basketball"
+            },
+            {
+              "id": 1,
+              "hobby": "football"
+            }
+          ]
+        },
+        {
+          "id": 1,
+          "name": "Theresia",
+          "isActive": false,
+          "favoriteFruit": "apple",
+          "gender": "male",
+          "hobby": [
+            {
+              "id": 2,
+              "hobby": "baseball"
+            },
+            {
+              "id": 1,
+              "hobby": "football"
+            }
+          ]
+        },
+        {
+          "id": 2,
+          "name": "Theresia",
+          "isActive": false,
+          "favoriteFruit": "strawberry",
+          "gender": "male",
+          "hobby": [
+            {
+              "id": 1,
+              "hobby": "baseball"
+            },
+            {
+              "id": 1,
+              "hobby": "baseball"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "_id": "6410370d6d2085e6d68532e0",
+      "age": 33,
+      "eyeColor": "brown",
+      "name": "Andrews Winters",
+      "address": "Jakarta",
+      "registered": "2023-02-19T11:13:58 -07:00",
+      "friends": [
+        {
+          "id": 0,
+          "name": "Theresia",
+          "isActive": true,
+          "favoriteFruit": "apple",
+          "gender": "male",
+          "hobby": [
+            {
+              "id": 1,
+              "hobby": "basketball"
+            },
+            {
+              "id": 2,
+              "hobby": "baseball"
+            }
+          ]
+        },
+        {
+          "id": 1,
+          "name": "Theresia",
+          "isActive": false,
+          "favoriteFruit": "apple",
+          "gender": "male",
+          "hobby": [
+            {
+              "id": 2,
+              "hobby": "basketball"
+            },
+            {
+              "id": 2,
+              "hobby": "football"
+            }
+          ]
+        },
+        {
+          "id": 2,
+          "name": "Theresia",
+          "isActive": false,
+          "favoriteFruit": "strawberry",
+          "gender": "male",
+          "hobby": [
+            {
+              "id": 2,
+              "hobby": "baseball"
+            },
+            {
+              "id": 1,
+              "hobby": "football"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "_id": "6410370d47961a96ce587b99",
+      "age": 33,
+      "eyeColor": "blue",
+      "name": "Britney Bradford",
+      "address": "Bandung",
+      "registered": "2021-12-24T06:12:00 -07:00",
+      "friends": [
+        {
+          "id": 0,
+          "name": "Theresia",
+          "isActive": true,
+          "favoriteFruit": "banana",
+          "gender": "female",
+          "hobby": [
+            {
+              "id": 2,
+              "hobby": "baseball"
+            },
+            {
+              "id": 2,
+              "hobby": "football"
+            }
+          ]
+        },
+        {
+          "id": 1,
+          "name": "Theresia",
+          "isActive": false,
+          "favoriteFruit": "banana",
+          "gender": "female",
+          "hobby": [
+            {
+              "id": 1,
+              "hobby": "basketball"
+            },
+            {
+              "id": 1,
+              "hobby": "football"
+            }
+          ]
+        },
+        {
+          "id": 2,
+          "name": "Theresia",
+          "isActive": false,
+          "favoriteFruit": "banana",
+          "gender": "female",
+          "hobby": [
+            {
+              "id": 2,
+              "hobby": "basketball"
+            },
+            {
+              "id": 1,
+              "hobby": "football"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "_id": "6410370d1ade118454cf7e08",
+      "age": 29,
+      "eyeColor": "green",
+      "name": "Higgins Petersen",
+      "address": "Bandung",
+      "registered": "2021-09-22T12:24:46 -07:00",
+      "friends": [
+        {
+          "id": 0,
+          "name": "Farid",
+          "isActive": true,
+          "favoriteFruit": "apple",
+          "gender": "male",
+          "hobby": [
+            {
+              "id": 1,
+              "hobby": "baseball"
+            },
+            {
+              "id": 1,
+              "hobby": "basketball"
+            }
+          ]
+        },
+        {
+          "id": 1,
+          "name": "Farid",
+          "isActive": false,
+          "favoriteFruit": "banana",
+          "gender": "male",
+          "hobby": [
+            {
+              "id": 2,
+              "hobby": "basketball"
+            },
+            {
+              "id": 2,
+              "hobby": "baseball"
+            }
+          ]
+        },
+        {
+          "id": 2,
+          "name": "Theresia",
+          "isActive": false,
+          "favoriteFruit": "banana",
+          "gender": "male",
+          "hobby": [
+            {
+              "id": 1,
+              "hobby": "basketball"
+            },
+            {
+              "id": 2,
+              "hobby": "baseball"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "_id": "6410370dbf15a4d38650c187",
+      "age": 35,
+      "eyeColor": "brown",
+      "name": "Augusta Mathis",
+      "address": "Bali",
+      "registered": "2022-07-07T02:39:38 -07:00",
+      "friends": [
+        {
+          "id": 0,
+          "name": "Farid",
+          "isActive": false,
+          "favoriteFruit": "apple",
+          "gender": "female",
+          "hobby": [
+            {
+              "id": 1,
+              "hobby": "football"
+            },
+            {
+              "id": 2,
+              "hobby": "basketball"
+            }
+          ]
+        },
+        {
+          "id": 1,
+          "name": "Theresia",
+          "isActive": true,
+          "favoriteFruit": "banana",
+          "gender": "female",
+          "hobby": [
+            {
+              "id": 2,
+              "hobby": "baseball"
+            },
+            {
+              "id": 2,
+              "hobby": "baseball"
+            }
+          ]
+        },
+        {
+          "id": 2,
+          "name": "Farid",
+          "isActive": true,
+          "favoriteFruit": "apple",
+          "gender": "female",
+          "hobby": [
+            {
+              "id": 2,
+              "hobby": "football"
+            },
+            {
+              "id": 1,
+              "hobby": "football"
+            }
+          ]
+        }
+      ]
+    }
+  ]
+
+
+//   wajib ada yang menggunakan array method FIND, FILTER atau MAP dalam pengerjaannya.
+
+// 1) display/print person yang registered dibawah tahun 2022
+// 2) display person yang address nya Bali 
+// 3) display friends yang hobby nya football
+// 4) display hobby dari friends id 2
+// 5) display friends yang gender nya male dan mempunya hobby basketball
+// 6) display friends yang isActive nya true dan gender nya female dan favorite fruit nya strawberry
+// 7) display siapa saja yang mempunya teman name Theresia
+// 8) display hobby id 1 dari friends yang isActive nya true
+// 9) display person yang eye color nya brown dan favorite fruit nya banana dan mempunyai teman hobby basketball
+// 10) display person yang mempunya friends gender male atau favorite fruit nya banana dan registered di tahun 2023
+
+
+
+// 1) display/print person yang registered dibawah tahun 2022
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~1.) print person yang registered dibawah tahun 2022 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`");
+
+let under2022 = (person.filter(function(xx) {
+    return parseInt(xx.registered.substring(0, 4)) < 2022
+}))
+
+console.table(under2022)
+//parseInt mengubah string menjadi integer
+//subString parameter yang menentukan mulai dan akhir suatu string
+
+
+
+
+
+
+
+// 2) display person yang address nya Bali 
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~2.) display person yang address nya Bali ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`");
+
+let bali = person.filter(function (a) {
+    return a.address === "Bali";
+})
+console.table(bali);
+
+
+
+
+
+
+
+
+// 3) display friends yang hobby nya football
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~3.) display friends yang hobby nya football ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`");
+
+let football = person.flatMap(function(x) {
+    return x.friends.filter(function(y) {
+      return y.hobby.find(function(z) {
+        return z.hobby === "football"
+      })
+    })
+  })
+  
+  console.table(football);
+
+
+
+
+
+
+
+// 4) display hobby dari friends id 2
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~4.) display hobby dari friends id 2 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`");
+
+
+let hobbyId = person.flatMap (function(r) {
+    return r.friends.filter(function(r2){
+        return r2.id === 2
+    })
+})
+console.table(hobbyId);
+
+
+
+
+
+
+// 5) display friends yang gender nya male dan mempunyai hobby basketball
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~5.) display friends yang gender nya male dan mempunyai hobby basketball ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`");
+
+let maleBasketball = person.flatMap(function(x1) {
+    return x1.friends.filter(function(y1) {
+        return y1.gender === "male" && y1.hobby.find(function(z1) {
+            return z1.hobby === "basketball";
+        });
+    });
+});
+
+console.table(maleBasketball);
+
+
+
+
+
+
+// 6) display friends yang isActive nya true dan gender nya female dan favorite fruit nya strawberry
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  6) display friends yang isActive nya true dan gender nya female dan favorite fruit nya strawberry ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`");
+let isActiveFemaleStrawberry = person.flatMap(function(x2) {
+    return x2.friends.filter(function(y2) {
+        return y2.gender === "female" && y2.isActive === true && y2.favoriteFruit === "strawberry" && y2.hobby.find(function(z2) {
+            return z2.hobby === "basketball";
+        });
+    });
+});
+
+console.table(isActiveFemaleStrawberry);
+
+
+
+
+
+
+// 7) display siapa saja yang mempunya teman name Theresia
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  7) display siapa saja yang mempunya teman name Theresia ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`");
+
+let there = person.filter(function (b) {
+    return b.friends.find(function (c) {
+        return c.name === "Theresia"
+    })
+})
+console.table(there);
+
+
+
+
+
+
+// 8) display hobby id 1 dari friends yang isActive nya true
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 8.) display hobby id 1 dari friends yang isActive nya true ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`");
+
+
+
+// 9) display person yang eye color nya brown dan favorite fruit nya banana dan mempunyai teman hobby basketball
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  9) display person yang eye color nya brown dan favorite fruit nya banana dan mempunyai teman hobby basketball ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`");
+
+
+
+
+// 10) display person yang mempunyai friends gender male atau favorite fruit nya banana dan registered di tahun 2023
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  9) display person yang eye color nya brown dan favorite fruit nya banana dan mempunyai teman hobby basketball ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`");
+
+
+
+
+
+
+
+
+
+
+
